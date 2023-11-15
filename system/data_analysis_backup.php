ed32a65bb286d5feb679a9654b8261aa18c16a6b
MrBubune <51889542+MrBubune@users.noreply.github.com>
2023-11-15 12:46:51 +0000

    Update data_analysis_backup.php

--- system/data_analysis_backup.php
+++ system/data_analysis_backup.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+
+// Check if the user is not logged in
+if (!isset($_SESSION['username'])) {
+    // Redirect to the login page
+    header('Location: login.php');
+    exit;
+}
 // Database configuration
 $servername = "localhost";
 $username = "root";
